9f6927f090567504de6b4f1d49d01bed1fa0c0d5
kristina kolesarova <kristina.kolesarova@akademiasovy.sk>
2017-12-04 23:25:32 +0100

    added function

--- TicTacToe/script/script.js
+++ TicTacToe/script/script.js
@@ -1,3 +1,32 @@
-function clickimg() {
-	console.log(this);
-}
+var player=1;
+      function clickimg(value) {
+        console.log(value);
+        var row=Math.floor(value/10);
+        var col=value%10;
+ if (player==1) {
+        console.log(value);
+        var row=Math.floor(value/10);
+        var col=value%10;
+        var imageID="img"+row+col;
+        document.getElementById(imageID).src = "img/x.png" ;
+   }
+ 
+ else {
+        console.log(value);
+        var row=Math.floor(value/10);
+        var col=value%10;
+        var imageID="img"+row+col;
+        document.getElementById(imageID).src = "img/o.png" ;
+   }
+    nextPlayer();
+  }
+  function nextPlayer(){
+    if (player == 1) {
+    player = 2;
+    
+  }
+  else {
+    player = 1;
+ 
+  }
+  }
