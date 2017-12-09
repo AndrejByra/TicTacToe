920356b647c2b850f0f42ea6301902c204b0cb02
kristina kolesarova <kristina.kolesarova@akademiasovy.sk>
2017-12-09 23:16:30 +0100

    added function printMessage and removeMessage for the player1,player2

--- TicTacToe/script/script.js
+++ TicTacToe/script/script.js
@@ -1,13 +1,5 @@
 var player=1;
-var pic1 = document.getElementById("img00").src;
-var pic2 = document.getElementById("img01").src;
-var pic3 = document.getElementById("img02").src;
-var pic4 = document.getElementById("img10").src;
-var pic5 = document.getElementById("img11").src;
-var pic6 = document.getElementById("img12").src;
-var pic7 = document.getElementById("img20").src;
-var pic8 = document.getElementById("img21").src;
-var pic9 = document.getElementById("img22").src;
+
 
 function clickimg(value) {
         var row=Math.floor(value/10);
@@ -31,29 +23,12 @@ function clickimg(value) {
    }
     nextPlayer();
     whoNext();
-    detectWin();
-   // alertWin();
+     }
+      
+    
 
-  }
-  function detectWin()
-{
-    if ( pic1 ==  pic2 ==  pic3){
-          console.log("prvyriadok");
-    }
-    else if ( pic4 ==  pic5 &&  pic6)
-    {
-        console.log("druhyriadok");
-    }
-    else {
-        console.log("nothing");
-    }
-
-}
-  function alertWin(){
-
-     alert($("#Player1").val());
-
-  }
+ 
+  
   function nextPlayer(){
     if (player == 1) {
     player = 2;
@@ -112,11 +87,13 @@ function whoNext(){
 function printMessage(){
  
             document.getElementById("message1").innerHTML="double-click to change the name";
-            document.getElementById("message2").innerHTML="double-click to change the name"; } 
+            document.getElementById("message2").innerHTML="double-click to change the name"; 
+          } 
 
 function removeMessage(){
    
             document.getElementById("message1").innerHTML=" "; 
-            document.getElementById("message2").innerHTML=" "; }
+            document.getElementById("message2").innerHTML=" ";
+             }
              
    
